15c4fada39d0cd1d82fa0227b0a073ca056044ce
Your Name <nazik_111@mail.ru>
2019-04-20 11:20:24 +0600

    add "comment add and remove"

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -152,4 +152,76 @@ router.post(
   }
 );
 
+//@route  POST api/posts/comment/:id
+//@desc   Add comment to post
+//@access Private
+
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id).then(post => {
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+      };
+
+      // Add to comment array
+      post.comment.unshift(newComment);
+
+      // Save comment
+      post
+        .save()
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+    });
+  }
+);
+
+//@route  DELETE api/posts/comment/:id/:comment_id
+//@desc   Remove comment from post
+//@access Private
+
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id).then(post => {
+      // Check if comment exists
+      if (
+        post.comment.filter(com => com._id.toString() === req.params.comment_id)
+          .length === 0
+      ) {
+        return res
+          .status(404)
+          .json({ commentnotexists: 'Comment does not exist' });
+      }
+
+      // Get remove index
+      const removeIndex = post.comment
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id);
+
+      // Splice comment out of array
+      post.comment.splice(removeIndex, 1);
+
+      // Save comment
+      post
+        .save()
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+    });
+  }
+);
+
 module.exports = router;
