79c4465f7258bdee11088ffcf91db28c25442303
Your Name <nazik_111@mail.ru>
2019-06-08 19:44:39 +0600

    Prepared for deploy

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const profile = require('./routes/api/profile');
 const words = require('./routes/api/words');
 const posts = require('./routes/api/posts');
 const passport = require('passport');
+const path = require('path');
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -29,6 +30,14 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/words', words);
 app.use('/api/posts', posts);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
