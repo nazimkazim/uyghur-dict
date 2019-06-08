185e899062e78c1bec22e2f6b4f33662d8024a95
Your Name <nazik_111@mail.ru>
2019-06-08 21:57:34 +0600

    add "change server back to 5000"

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 //log.Fatal(http.ListenAndServe(':' + os.Getenv('PORT'), router));
 app.listen(port, () => console.log(`Server running on port ${port}`));
